a05e0af3d44838e94266555dd86e0625bb8f0591
cecil john tantay <cjbtantay@gmail.com>
2019-02-11 14:56:16 -0800

    finish post request and get request

--- index.js
+++ index.js
@@ -7,21 +7,34 @@ server.use(express.json());
 
 server.post('/api/users', (req, res) => {
   const user = req.body;
+
   db.insert(user)
     .then(user => {
-      if (!user.name || !user.bio) {
+      if (!user) {
         res.status(400).json({
           success: false,
           message: 'Please provide name and bio for the user.'
         });
+      } else {
+        res.status(201).json({ success: true, user });
       }
-      res.status(201).json({ success: true, user });
     })
-    .catch(err => {
+    .catch(() => {
       res.status(500).json({
-        success: false,
         error: 'There was an error while saving the user to the database'
       });
+    });
+});
+
+server.get('/api/users', (req, res) => {
+  db.find()
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(() => {
+      res
+        .status(500)
+        .json({ error: 'The users information could not be retrieved.' });
     });
 });
 
