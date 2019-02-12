c4db3fa4e35e6597d9355f62220d1fa093dfe62c
cecil john tantay <cjbtantay@gmail.com>
2019-02-11 19:20:00 -0800

    fix delete

--- index.js
+++ index.js
@@ -46,14 +46,148 @@ server.get('/api/users/:id', (req, res) => {
         res
           .status(404)
           .json({ message: 'the user with the specified ID does not exist.' });
+      } else {
+        res.status(200).json({ success: true, user });
       }
-      res.status(200).json({ success: true, user });
     })
     .catch(() => {
       res
         .status(500)
         .json({ error: 'The user information could not be retrieved.' });
     });
+});
+
+server.delete('/api/users/:id', (req, res) => {
+  const userId = req.params.id;
+
+  db.findById(userId)
+    .then(response => {
+      db.remove(userId)
+        .then(() => res.status(200).json(response))
+        .catch(() =>
+          res.status(500).json({ error: 'The user could not be removed' })
+        );
+    })
+    .catch(error =>
+      res
+        .status(404)
+        .json({ message: 'The user with the specified ID does not exist' })
+    );
+
+  // db.findById(userId).then(() => {
+  //   if (!userId) {
+  //     res
+  //       .status(404)
+  //       .json({ message: 'the user with the specified ID does not exist.' });
+  //   } else {
+  //     db.remove(userId).then(() => {
+  //       db.findById(userId)
+  //       .then(deleted => {
+  //         res.status(200).json({ deleted })
+  //       })
+
+  //     });
+  //   }
+  // });
+
+  // db.remove(userId)
+  //   .then(deleted => {
+  //     if (!deleted) {
+  //       res
+  //         .status(404)
+  //         .json({ message: 'the user with the specified ID does not exist.' });
+  //     } else {
+  //       db.findById(userId).then(deletedUser => {
+  //         console.log(deletedUser);
+  //         res.status(200).json({ deletedUser });
+  //       });
+  //       // res.status(200).json({ deleted });
+  //     }
+  //   })
+  //   .catch(() => {
+  //     res.status(500).json({ error: 'The user could not be removed' });
+  //   });
+  // xxxxx
+  // if (!userId) {
+  //   res
+  //     .status(404)
+  //     .json({ message: 'The user with the specified ID does not exist.' });
+  // } else if (!user) {
+  //   res
+  //     .status(400)
+  //     .json({ errorMessage: 'Please provide name and bio for the user.' });
+  // } else {
+  //   db.update(userId, user)
+  //     .then(() => {
+  //       db.findById(userId).then(updatedUser => {
+  //         res.status(200).json({ updatedUser });
+  //       });
+  //     })
+  //     .catch(() => {
+  //       res
+  //         .status(500)
+  //         .json({ error: 'The user information could not be modified.' });
+  //     });
+  // }
+});
+
+server.put('/api/users/:id', (req, res) => {
+  const userId = req.params.id;
+  const user = req.body;
+
+  db.findById(userId)
+    .then(userName => {
+      if (!userName) {
+        res
+          .status(404)
+          .json({ message: 'The user with the specified ID does not exist.' });
+      } else {
+        if (!user.name || !user.bio) {
+          res.status(400).json({
+            errorMessage: 'please provide name and bio for the user.'
+          });
+        } else {
+          db.update(userId, user)
+            .then(() => {
+              db.findById(userId).then(updatedUser => {
+                res.status(200).json(updatedUser);
+              });
+            })
+            .catch(() =>
+              res
+                .status(500)
+                .json({ error: 'The user information could not be modified.' })
+            );
+        }
+      }
+    })
+    .catch(() =>
+      res
+        .status(404)
+        .json({ message: 'The user with the specified ID does not exist.' })
+    );
+
+  // if (!userId) {
+  //   res
+  //     .status(404)
+  //     .json({ message: 'The user with the specified ID does not exist.' });
+  // } else if (!user) {
+  //   res
+  //     .status(400)
+  //     .json({ errorMessage: 'Please provide name and bio for the user.' });
+  // } else {
+  //   db.update(userId, user)
+  //     .then(() => {
+  // db.findById(userId).then(updatedUser => {
+  //   res.status(200).json({ updatedUser });
+  // });
+  //     })
+  //     .catch(() => {
+  //       res
+  //         .status(500)
+  //         .json({ error: 'The user information could not be modified.' });
+  //     });
+  // }
 });
 
 server.listen(3333, () => {
